65c5416cc546c6f74e64fc9de9510ca053450488
Kyle McCall <kylem147@gmail.com>
2022-09-30 16:30:15 -0700

    deletes unused file

--- src/services/pokemon.js
+++
@@ -1,9 +0,0 @@
-export async function fetchPokemon() {
-  // const params = new URLSearchParams();
-
-  
-
-  const resp = await fetch(`https://pokedex-alchemy.herokuapp.com/api/pokedex`);
-  const data = await resp.json();
-  return data.results;
-}
